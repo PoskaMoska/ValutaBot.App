616bdc57e346985f844ab97b79069976c77b25b5
Bural <bural@ukr.net>
2026-09-23 21:37:13 +0300

    chore: remove test scripts

--- gap_check.py
+++
@@ -1,5 +0,0 @@
-import os, psycopg2
-conn = psycopg2.connect(os.getenv('DATABASE_URL'))
-c = conn.cursor()
-c.execute("SELECT COUNT(*) FROM historical_candles WHERE asset='EURUSD' AND open_time > '2026-09-12 08:37:00' AND open_time < '2026-09-20 00:00:00'")
-print('Gap Candles:', c.fetchone()[0])
--- scratch/extract.py
+++
@@ -1,8 +0,0 @@
-import re
-import os
-
-source_file = 'ml_service/model.py'
-with open(source_file, 'r', encoding='utf-8') as f:
-    content = f.read()
-
-# I will just write the data loader manually to be safer, no need for complex regex.
